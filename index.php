c8585291896597d29b0ba0212e59d5c9601abe07
camzu98 <kamillanger4@gmail.com>
2017-06-04 17:31:20 +0200

    START

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Strona główna</title>
+        <meta name="description" content="Ośrodek Szkolenia Kierowców">
+        <meta name="author" content="Kamil Langer">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="//fonts.googleapis.com/css?family=Raleway:400,300,600" rel="stylesheet" type="text/css">
+        <link href="https://fonts.googleapis.com/css?family=Revalia" rel="stylesheet">
+        <script src="js/jquery-3.1.1.min.js"></script>
+        <script src="js/snackbar.js"></script>
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/skeleton.css">
+        <link rel="stylesheet" href="css/index.css">
+        <link rel="stylesheet" href="css/snackbar.css">
+    </head>
+    <body>
+
+    </body>
+</html>
